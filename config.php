22c9b9fbb8a09a68adbe40fa15a9d0f1d39be38b
rockxprophet <rockxprophet@users.noreply.github.com>
2015-04-03 21:19:51 -0500

    Delete config.php

--- config.php
+++
@@ -1,8 +0,0 @@
-<?php
-	$dbUser = "jhfrye";
-	$dbPassword = "q.6u+0T9sdhU";
-	$dbName = "db_jhfrye";
-	$dbHost = "dbdev.cs.uiowa.edu";
-	$adminEmail = "joshua-frye@uiowa.edu";
-	$baseURL = "http://webdev.divms.uiowa.edu/~jhfrye/phpdb";
-?>
